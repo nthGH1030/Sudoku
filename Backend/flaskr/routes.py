5008c289d1d28706fa53f2bb4decc961bee7c883
Ngan Tsz Hang <nthGH1030@gmail.com>
2023-10-18 23:12:29 +0800

    register, login, logout routes and htmls implemented

--- Backend/flaskr/routes.py
+++ Backend/flaskr/routes.py
@@ -1,8 +1,9 @@
-from flask import Flask, flash, redirect, render_template, request, session, url_for, Blueprint
+from flask import Flask, flash, redirect, render_template, g,request, session, url_for, Blueprint
 from werkzeug.security import check_password_hash, generate_password_hash
 from . import solver
 from flaskr.db import get_db
 import random
+import functools
 
 bp = Blueprint('routes',__name__)
 
@@ -12,32 +13,7 @@ def index():
 
 @bp.route("/game", methods= ["POST","GET"])
 def game():
-    '''
-    rowInitial = "ABCDEFGHI"
-    colInitial = "123456789"
-    squares = []
-    questionGrid = {}
-    answerGrid = {}
 
-    question = session.get("question")
-    answer = session.get("answer")
-
-    if "question" not in session or "answer" not in session:
-        question, answer = solver.solve()
-        session["question"] = question
-        session["answer"] = answer
-
-    index = 0
-    for char in rowInitial:
-        for digit in colInitial:
-            squares.append(char + digit)
-            questionGrid[char + digit] = question[index]
-            answerGrid[char + digit] = answer[index]
-            index += 1
-    
-    session["answerGrid"] = answerGrid
-    session["questionGrid"] = questionGrid
-    '''
     # thins that got run no matter its get or post
     rowInitial = "ABCDEFGHI"
     colInitial = "123456789"
@@ -101,10 +77,6 @@ def game():
         session["message"] = message
         
         return redirect(url_for("routes.answer"))
-
-        #check what the answer is in console
-        #print("this is the answer")
-        #print(answerGrid)
         
     return render_template("game.html", question_dict = question_dict, 
                            rowInitial = rowInitial, colInitial = colInitial)
@@ -156,8 +128,89 @@ def answer():
     print(answer_dict)
 
 
-
-    
     return render_template("answer.html", userAnsDict = userAnsDict, message = message, 
                            answer_dict = answer_dict, question_dict = question_dict
                            ,rowInitial = rowInitial, colInitial = colInitial)
+
+@bp.route("/register", methods= ["POST","GET"])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template("register.html")
+
+@bp.route("/login", methods= ["POST","GET"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template("login.html")
+
+#Check if user is still in session
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+#logout when session is cleared
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('routes.index'))
+
+#Require user to login before creating, editing and deleting blog posts
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('routes.index'))
+
+        return view(**kwargs)
+
+    return wrapped_view
